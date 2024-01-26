d086cad2959720ae0366a700150e5196f1f928af
FerDMS <fernandodms04@gmail.com>
2024-01-25 22:39:51 -0600

    Update for Feb.-Jun. 2024 Projects

--- main.py
+++ main.py
@@ -26,175 +26,45 @@ def requestData():
 
     # Request cookies that define the necessary tokens to access the information
     cookies = {
-        '_gid': 'GA1.3.1220078569.1702573968',
-        '1P_JAR': '2023-12-14-17',
-        'AEC': 'Ackid1T-hJYX-Ts8fpUACwrlbmef1WX_iycdo4aGyz9hlzekzETxjQW4oAQ',
-        'NID': '511=E5zGmolfgRzs58YNJHmiGepGegVw2pm-2wJ-6qccnm4sE5JlML9RqXhrdsyYgyXYOpl01wZLXqYlJAgA8x58DlpPVs6mR_FbyOYvUOWM3vcwe3EQ-pWT-7P_GxQdVEx8etUE3lX1hw9oZnaW8uvfXKxgBfLHoRX1iHRd0olLo3LgvCTzoS23v3NtNHfpJWs-W3rWJdo2kI4',
-        '_ga': 'GA1.3.1741927020.1702573966',
-        '_gat_marketingTracker': '1',
-        '_gat': '1',
-        '_ga_S4FJY0X3VX': 'GS1.1.1702573965.1.1.1702576195.0.0.0',
-    }
+    "NID": "511=TJtaE6hIQ5rTW0K77jRvDIjbcoZlMvq1vXs0YbrF8ePc8anb__R6zQHrtSFLYFPIdHL9S1dZYgkNw_w3DB32-R3UmYI1wOmqti-_NOAQKQamGaiz8FvO5eJwkq00E0IQKXUMr4dcjGCZCNa63iMS9enKCJvw-abBe4orPkYSZo0",
+    "_gid": "GA1.3.1078430438.1706232920",
+    "_ga": "GA1.3.826474088.1706232920",
+    "_gat_marketingTracker": "1",
+    "_gat": "1",
+    "_ga_S4FJY0X3VX": "GS1.1.1706232920.1.0.1706232922.0.0.0",
+}
 
     # Request headers with information about the request and the client
     headers = {
-        'authority': 'lookerstudio.google.com',
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en',
-        'content-type': 'application/json',
-        # 'cookie': '_gid=GA1.3.1220078569.1702573968; 1P_JAR=2023-12-14-17; AEC=Ackid1T-hJYX-Ts8fpUACwrlbmef1WX_iycdo4aGyz9hlzekzETxjQW4oAQ; NID=511=E5zGmolfgRzs58YNJHmiGepGegVw2pm-2wJ-6qccnm4sE5JlML9RqXhrdsyYgyXYOpl01wZLXqYlJAgA8x58DlpPVs6mR_FbyOYvUOWM3vcwe3EQ-pWT-7P_GxQdVEx8etUE3lX1hw9oZnaW8uvfXKxgBfLHoRX1iHRd0olLo3LgvCTzoS23v3NtNHfpJWs-W3rWJdo2kI4; _ga=GA1.3.1741927020.1702573966; _gat_marketingTracker=1; _gat=1; _ga_S4FJY0X3VX=GS1.1.1702573965.1.1.1702576195.0.0.0',
-        'encoding': 'null',
-        'origin': 'https://lookerstudio.google.com',
-        'referer': 'https://lookerstudio.google.com/reporting/1e2031bb-c1c2-4834-8c67-668d432e1a12/page/p_beb89xjkcd?s=sIOHo74qPCY',
-        'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-    }
+    "authority": "lookerstudio.google.com",
+    "method": "POST",
+    "path": "/batchedDataV2?appVersion=20240123_0800",
+    "scheme": "https",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en",
+    "content-length": "1997",
+    "content-type": "application/json",
+    #"cookie": "NID=511=TJtaE6hIQ5rTW0K77jRvDIjbcoZlMvq1vXs0YbrF8ePc8anb__R6zQHrtSFLYFPIdHL9S1dZYgkNw_w3DB32-R3UmYI1wOmqti-_NOAQKQamGaiz8FvO5eJwkq00E0IQKXUMr4dcjGCZCNa63iMS9enKCJvw-abBe4orPkYSZo0; _gid=GA1.3.1078430438.1706232920; _ga=GA1.3.826474088.1706232920; _gat_marketingTracker=1; _gat=1; _ga_S4FJY0X3VX=GS1.1.1706232920.1.0.1706232922.0.0.0",
+    "encoding": "null",
+    "origin": "https://lookerstudio.google.com",
+    "referer": "https://lookerstudio.google.com/reporting/1e2031bb-c1c2-4834-8c67-668d432e1a12/page/p_povubyjdcd?s=sIOHo74qPCY%5C",
+    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
 
-    # Request parameters (query parameters that could also be specified in the URL)
+    # # Request parameters (query parameters that could also be specified in the URL)
     params = {
         'appVersion': '20231211_0700',
     }
 
     # Request JSON data or payload which specified the amount of rows to obtain
-    json_data = {
-        'dataRequest': [
-            {
-                'requestContext': {
-                    'reportContext': {
-                        'reportId': '1e2031bb-c1c2-4834-8c67-668d432e1a12',
-                        'pageId': 'p_beb89xjkcd',
-                        'mode': 1,
-                        'componentId': 'cd-fma89xjkcd',
-                        'displayType': 'simple-table',
-                    },
-                    'requestMode': 0,
-                },
-                'datasetSpec': {
-                    'dataset': [
-                        {
-                            'datasourceId': '57f0958f-190f-47b9-a937-4efa365d5143',
-                            'revisionNumber': 0,
-                            'parameterOverrides': [],
-                        },
-                    ],
-                    'queryFields': [
-                        {
-                            'name': 'qt_n0b74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_39243438_',
-                            },
-                        },
-                        {
-                            'name': 'qt_o0b74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_n341627934_',
-                            },
-                        },
-                        {
-                            'name': 'qt_p0b74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_n1986575415_',
-                            },
-                        },
-                        {
-                            'name': 'qt_isc74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_1169845789_',
-                            },
-                        },
-                        {
-                            'name': 'qt_jsc74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_1336977314_',
-                            },
-                        },
-                        {
-                            'name': 'qt_ksc74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_n1368112855_',
-                            },
-                        },
-                        {
-                            'name': 'qt_lsc74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_n120571253_',
-                            },
-                        },
-                    ],
-                    'sortData': [
-                        {
-                            'sortColumn': {
-                                'name': 'qt_o0b74z9kcd',
-                                'datasetNs': 'd0',
-                                'tableNs': 't0',
-                                'dataTransformation': {
-                                    'sourceFieldName': '_n341627934_',
-                                },
-                            },
-                            'sortDir': 0,
-                        },
-                    ],
-                    'includeRowsCount': True,
-                    'relatedDimensionMask': {
-                        'addDisplay': False,
-                        'addUniqueId': False,
-                        'addLatLong': False,
-                    },
-                    'paginateInfo': {
-                        'startRow': 1,
-                        'rowsCount': rows,
-                    },
-                    'filters': [],
-                    'features': [],
-                    'dateRanges': [],
-                    'contextNsCount': 1,
-                    'dateRangeDimensions': [
-                        {
-                            'name': 'qt_6be74z9kcd',
-                            'datasetNs': 'd0',
-                            'tableNs': 't0',
-                            'dataTransformation': {
-                                'sourceFieldName': '_1176618056_',
-                                'transformationConfig': {
-                                    'transformationType': 5,
-                                },
-                            },
-                        },
-                    ],
-                    'calculatedField': [],
-                    'needGeocoding': False,
-                    'geoFieldMask': [],
-                    'multipleGeocodeFields': [],
-                },
-                'role': 'main',
-                'retryHints': {
-                    'useClientControlledRetry': True,
-                    'isLastRetry': False,
-                    'retryCount': 0,
-                    'originalRequestId': 'cd-fma89xjkcd_0_0',
-                },
-            },
-        ],
-    }
+    json_data = {"dataRequest":[{"requestContext":{"reportContext": {"reportId":"1e2031bb-c1c2-4834-8c67-668d432e1a12","pageId":"p_povubyjdcd","mode":1,"componentId":"cd-108tm1jdcd","displayType":"simple-table"},"requestMode":0},"datasetSpec":{"dataset":[{"datasourceId":"d68ea43a-2f0e-4a00-8898-22f8a47ca430","revisionNumber":0,"parameterOverrides":[]}],"queryFields":[{"name":"qt_fh7tm1jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_985914030_"}},{"name":"qt_utxp42jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_n1124254750_"}},{"name":"qt_4ai252jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_n414281798_"}},{"name":"qt_8ygnj7jdcd","datasetNs":"d0","tableNs":"t0","resultTransformation":{"analyticalFunction":0,"isRelativeToBase":False},"dataTransformation":{"sourceFieldName":"_n179593893_"}},{"name":"qt_7328s6jdcd","datasetNs":"d0","tableNs":"t0","resultTransformation":{"analyticalFunction":0,"isRelativeToBase":False},"dataTransformation":{"sourceFieldName":"_302064841_"}},{"name":"qt_0qpj82jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_n1735679163_"}},{"name":"qt_2rppq3jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_n920199899_"}},{"name":"qt_znceizkdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_n1927870623_"}}],"sortData":[{"sortColumn":{"name":"qt_fh7tm1jdcd","datasetNs":"d0","tableNs":"t0","dataTransformation":{"sourceFieldName":"_985914030_"}},"sortDir":1}],"includeRowsCount":True,"relatedDimensionMask":{"addDisplay":False,"addUniqueId":False,"addLatLong":False},"paginateInfo":{"startRow":1,"rowsCount":rows},"filters":[],"features":[],"dateRanges":[],"contextNsCount":1,"calculatedField":[],"needGeocoding":False,"geoFieldMask":[],"multipleGeocodeFields":[]},"role":"main","retryHints":{"useClientControlledRetry":True,"isLastRetry":False,"retryCount":0,"originalRequestId":"cd-108tm1jdcd_0_0"}}]}
 
     # URL for the connection
     url = "https://lookerstudio.google.com/batchedDataV2"
@@ -232,7 +102,7 @@ def parseData_csv(file_name):
     with open("response.txt", "r", encoding="utf-8") as file:
         response = file.read()
         print("Searching for matching JSON object...")
-        json_data = re.findall('(\{.*$)', response) 
+        json_data = re.findall(r'(\{.*$)', response) 
         print("JSON found, parsing the data...")
 
     # Parse the JSON response
@@ -242,19 +112,24 @@ def parseData_csv(file_name):
     columns = parsed_json['dataResponse'][0]['dataSubset'][0]['dataset']['tableDataset']['column']
 
     # Number of rows to visualize (not including headers)
-    n = len(columns[0]['stringColumn']['values'])
+    n = parsed_json['dataResponse'][0]['dataSubset'][0]['dataset']['tableDataset']['totalCount']
     print(f"Data parsed, found {n} entries")
 
     # Initialize text variable with headers
-    text = "Periodo,Matrícula,Nombre,Institución,Nombre del proyecto,Duración,Modalidad\r\n"
+    text = "Periodo,Matrícula,Nombre,Semestre,Carrera,Institución,Proyecto,Duración\n"
     print("Preparing .csv file...")
 
     # Scrap data and prepare it for CSV
-    for row in range(0, n):
-        for column in columns:
-            text += '"' + column['stringColumn']['values'][row] + '"' + ","
+    idx = {i : 0 for i in range(len(columns))}
+    for row in range(n):
+        for i, column in enumerate(columns):
+            if row in column['nullIndex']:
+                text += '"",'
+            else:
+                text += '"' + column['stringColumn']['values'][idx[i]] + '"' + ","
+                idx[i] += 1
         text = text[:len(text)-1]
-        text += "\r\n"
+        text += "\n"
 
     # Export formatted data to CSV
     with open(f"{file_name}.csv", "w", encoding="utf-8") as file:
@@ -275,41 +150,39 @@ def exportData_xlsx(file_name):
     df = pd.read_csv(f"{file_name}.csv")
 
     # Export formatted data to an excel file (.xlsx)
-    writer = pd.ExcelWriter(f'{file_name}.xlsx', engine='xlsxwriter') 
-    df.to_excel(writer, sheet_name=f"{file_name}", index=None, header=True)
+    with pd.ExcelWriter(f'{file_name}.xlsx', engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name=f"{file_name}", index=None, header=True)
 
-    # Declare instance of sheet as the active worksheet (to insert table)
-    ws = writer.sheets[f"{file_name}"]
+        # Declare instance of sheet as the active worksheet (to insert table)
+        ws = writer.sheets[f"{file_name}"]
 
-    # Get column names from DataFrame object and prepare them for table creation
-    # as dictionaries with a 'header' key as the column name
-    col_names = []
-    for col_name in df.columns:
-        col_names.append({'header' : col_name, 'text_wrap': True})
+        # Get column names from DataFrame object and prepare them for table creation
+        # as dictionaries with a 'header' key as the column name
+        col_names = []
+        for col_name in df.columns:
+            col_names.append({'header' : col_name, 'text_wrap': True})
 
-    # Get size of table (with headers) as (# of rows, # of columns)
-    size = {
-        'rows' : df.shape[0],
-        'columns' : df.shape[1]-1
-    }
+        # Get size of table (with headers) as (# of rows, # of columns)
+        size = {
+            'rows' : df.shape[0],
+            'columns' : df.shape[1]-1
+        }
 
-    # Create header objects as
-    ws.add_table(0, 0, size['rows'], size['columns'], {
-        'columns' : col_names,
-        'header_row' : True,
-        'banded_rows': True,
-        'style' : "Table Style Medium 2"
-    })
+        # Create header objects as
+        ws.add_table(0, 0, size['rows'], size['columns'], {
+            'columns' : col_names,
+            'header_row' : True,
+            'banded_rows': True,
+            'style' : "Table Style Medium 2"
+        })
 
-    # Autofit column widths
-    ws.autofit()
+        # Autofit column widths
+        ws.autofit()
 
-    # Save writer object and create Excel file
-    writer.save()
     print(f"Data saved to {file_name}.xlsx")
 
 
-file_name = "winter2024"
+file_name = "feb_jun_2024"
 requestData()
 parseData_csv(file_name)
 exportData_xlsx(file_name)
